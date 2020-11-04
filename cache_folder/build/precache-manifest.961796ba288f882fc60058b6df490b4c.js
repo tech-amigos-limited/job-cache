40d28d898a15a19e3d4e8efaec1f2b8f4bedebdc
Shashi Bangera <devops@techamigos.co>
2020-11-04 13:07:43 +0530

    testing with more files

---
+++ cache_folder/build/precache-manifest.961796ba288f882fc60058b6df490b4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab29230165de119062779627caa67a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85ddfe039a7ebffa7a5c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd6e5fa8992a826f5f04",
+    "url": "/static/js/2.25ef0bc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25ef0bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ddfe039a7ebffa7a5c",
+    "url": "/static/js/main.a4c986de.chunk.js"
+  },
+  {
+    "revision": "5f5c884615b91a15d3f9",
+    "url": "/static/js/runtime-main.7957723e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
